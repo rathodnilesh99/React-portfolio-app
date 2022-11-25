d338ecd7f3517b1d4fb327eaafd4ccf5760920f9
Nilesh Rathod <rathodnilesh999@gmail.com>
2022-11-25 22:27:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Sidebar from './components/sidebar';
 import Introduction from './components/introduction';
 import About from './components/about';
-import Timeline from './components/timeline';
 
 class App extends Component {
     render() {
